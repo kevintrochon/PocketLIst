b936e11e579058ad4b4f1c5688460b3437cc9029
kevin-trochon <trochonkevin@gmail.com>
2022-04-21 19:23:58 +1100

    Remaniement de la première vue.

--- app/src/main/java/nc/unc/ktrochon/pocketlist/MainActivity.java
+++ app/src/main/java/nc/unc/ktrochon/pocketlist/MainActivity.java
@@ -3,12 +3,10 @@ package nc.unc.ktrochon.pocketlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
